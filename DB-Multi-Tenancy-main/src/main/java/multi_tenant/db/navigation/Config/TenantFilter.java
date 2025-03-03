914833c39aafefaf272049400c19761fdcf723a5
ThuyKat <katiefirst7@gmail.com>
2025-03-03 14:46:23 +1100

    check for user-global header to set userRole in TenantContext

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Config/TenantFilter.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Config/TenantFilter.java
@@ -36,24 +36,38 @@ public class TenantFilter extends OncePerRequestFilter{
 			TenantService tenantService = tenantServiceProvider.getIfAvailable();
 
             if (tenantService == null) {
-                logger.error("TenantService is not available. Defaulting to DatabaseA.");
-                TenantContext.setCurrentTenant("databaseA");
+                logger.error("TenantService is not available. Defaulting to default db.");
+                TenantContext.setCurrentTenant("default");
+                TenantContext.setCurrentUserRole("DEVELOPER");
                 filterChain.doFilter(request, response);
                 return;
             }
 
             String shopName = request.getHeader("shop-name");
+            String userGlobalRole = request.getHeader("global-user");
           
 
             logger.info("Filter processing shop: {}", shopName);
+            
+            if (userGlobalRole !=null) {
+        		TenantContext.setCurrentUserRole(userGlobalRole);
+            }else {
+            	if(shopName == null) {
+            		//userGlobalRole == null and shopName == null
+            		TenantContext.setCurrentUserRole("DEVELOPER"); //default is developer if user does not define userRole
+            	}
+            		//userGlobalRole == null and shopName !=null ==> role of user is in tenant db
+            	
+            }
+            
 
-            if (shopName != null) {
-                String databaseName = tenantService.getDatabaseNameByShopId(shopName).getDbName();
-                TenantContext.setCurrentTenant(databaseName);
-                logger.info("Setting DB to: {} in thread: {}", databaseName, Thread.currentThread().getId());
-            } else {
+            if (shopName == null) {
                 logger.warn("Shop-name header missing");
                 TenantContext.setCurrentTenant("default");
+            } else {
+            	 String databaseName = tenantService.getDatabaseNameByShopId(shopName).getDbName();
+                 TenantContext.setCurrentTenant(databaseName);
+                 logger.info("Setting DB to: {} in thread: {}", databaseName, Thread.currentThread().getId());
             }
 
             filterChain.doFilter(request, response);
