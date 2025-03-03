227b95ef77fa68def6c42e8fd99aaabdd9a6915d
ThuyKat <katiefirst7@gmail.com>
2025-03-03 14:43:05 +1100

    added extracting userRole from jwt and set it into TenantContext

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Config/JwtFilter.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Config/JwtFilter.java
@@ -1,14 +1,18 @@
 package multi_tenant.db.navigation.Config;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.catalina.filters.ExpiresFilter.XServletOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -23,6 +27,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import multi_tenant.db.navigation.Service.CustomUserDetailsService;
 import multi_tenant.db.navigation.Utils.JwtUtil;
+import multi_tenant.db.navigation.Utils.TenantContext;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -39,6 +44,8 @@ public class JwtFilter extends OncePerRequestFilter {
 		final String authorizationHeader = request.getHeader("Authorization");
 		String username = null;
 		String jwt = null;
+		List<SimpleGrantedAuthority> authorities= new ArrayList<>();
+		
 		Map<String, Object> errorDetails = new HashMap<>();
 		try {
 			System.out.println("I am in jwt filter");
@@ -46,14 +53,25 @@ public class JwtFilter extends OncePerRequestFilter {
 			if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
 				jwt = authorizationHeader.substring(7);
 				username = jwtUtil.extractUsername(jwt);
+				authorities = jwtUtil.extractAuthorities(jwt);
+				System.out.println();
 			}
 			System.out.println("username : "+ username);
 			//validate jwt to bypass authentication
 			if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 				System.out.println("username is not null and no user been authenticated yet");
+				//update userRole
+				if (authorities.stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_OWNER")))  {
+					System.out.println("JwtFilter: setting role for user as owner");
+					TenantContext.setCurrentUserRole("OWNER");
+				}
 				// runtime poly
 				UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-				// verify if the token is validate
+				
+				System.out.println("JwtFilter:Double check user Roles after updated in tenant context:"+ userDetails.getAuthorities().stream().map(authority -> authority.getAuthority())
+	        .collect(Collectors.toList()));
+				
+				// verify if the token is validated
 				System.out.println("before validate token");
 				if (jwtUtil.validateToken(jwt, userDetails)) {
 					UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
@@ -63,8 +81,6 @@ public class JwtFilter extends OncePerRequestFilter {
 					//set userDetail, bypass authentication 
 					SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 					
-					System.out.println("Token is validated, done Jwt Filter...moving to successHandler? ");
-
 				}
 			}
 		} catch (Exception e) {
