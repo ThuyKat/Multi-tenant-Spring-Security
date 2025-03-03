3884ff188a6a037c6372266f1a1ac1b89481a292
ThuyKat <katiefirst7@gmail.com>
2025-03-03 14:56:09 +1100

    included variable and method to set and get userRole from Context

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Utils/TenantContext.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Utils/TenantContext.java
@@ -7,6 +7,7 @@ public class TenantContext {
 	private static final Logger logger = LoggerFactory.getLogger(TenantContext.class);
 
 	private static final ThreadLocal<String> CURRENT_TENANT = new ThreadLocal<>();
+	private static final ThreadLocal<String> CURRENT_ROLE = new ThreadLocal<>();
 
 	public static String getCurrentTenant() {
 		String tenant = CURRENT_TENANT.get();
@@ -14,13 +15,22 @@ public class TenantContext {
 		return tenant;
 	}
 	public static void setCurrentTenant(String databaseName) {
-		logger.info("database name: {}", databaseName);
 		System.out.println("I am in TenantContext, setting current tenant "+ databaseName+ "threadID: "+ Thread.currentThread().getId());
 		CURRENT_TENANT.set(databaseName);
 	}
 	
+	public static String getCurrentUserRole() {
+		String role = CURRENT_ROLE.get();
+		return role;
+	}
+	public static void setCurrentUserRole(String userRole) {
+		System.out.println("setting user role: "+ userRole);
+		CURRENT_ROLE.set(userRole);
+	}
+	
 	public static void clear() {
 		CURRENT_TENANT.remove();
+		CURRENT_ROLE.remove();
 	}
 	
 	}
