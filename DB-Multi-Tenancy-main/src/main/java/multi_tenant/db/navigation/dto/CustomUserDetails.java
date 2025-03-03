192baa99e740364d61a82f48768821d9a0da6932
ThuyKat <katiefirst7@gmail.com>
2025-03-03 14:57:06 +1100

    fixed constructor and setAuthorities method so it works with CustomuserDetailsService

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/dto/CustomUserDetails.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/dto/CustomUserDetails.java
@@ -19,27 +19,26 @@ public class CustomUserDetails implements UserDetails {
 	private String email;
     private String password;
     private List<GrantedAuthority> authorities = new ArrayList<>();
-    private boolean isGlobalUser;
     private String dbName;
     private Status status;
 
 
-    public CustomUserDetails(String email,String password,Status status,boolean isGlobalUser,String dbName) {
+    public CustomUserDetails(String email,String password,Status status,List<GrantedAuthority> authorities,String dbName) {
     	this.email = email;
     	this.password = password;
     	this.status = status;
-    	this.isGlobalUser=isGlobalUser;
+    	this.authorities = authorities;
     	this.dbName = dbName;
     	
     }
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		if(isGlobalUser) {
-			authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-		}else {
-	        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-
-		}
+//		if(isGlobalUser) { 
+//			authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));//OWNER or DEVELOPER, if its owner add ADMIN  as ROLE
+//		}else {
+//	        authorities.add(new SimpleGrantedAuthority("ROLE_USER")); //ADMIN or STAFF, get from entity's role
+//
+//		}
 		return authorities;
 	}
 
