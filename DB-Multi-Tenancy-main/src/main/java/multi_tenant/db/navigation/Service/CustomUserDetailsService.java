da6306e05cacc345b410eb495a98aa77f37620c7
ThuyKat <katiefirst7@gmail.com>
2025-03-03 14:53:37 +1100

    added authorities for user based on userRole

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Service/CustomUserDetailsService.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Service/CustomUserDetailsService.java
@@ -1,16 +1,23 @@
 package multi_tenant.db.navigation.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import multi_tenant.db.navigation.Entity.Global.Developer;
 import multi_tenant.db.navigation.Entity.Global.Owner;
 import multi_tenant.db.navigation.Entity.Tenant.TenantUser;
 import multi_tenant.db.navigation.Enum.Status;
-import multi_tenant.db.navigation.Repository.Global.GlobalUserRepository;
+import multi_tenant.db.navigation.Repository.Global.DeveloperRepository;
+import multi_tenant.db.navigation.Repository.Global.OwnerRepository;
 import multi_tenant.db.navigation.Repository.Tenant.TenantUserRepository;
 import multi_tenant.db.navigation.Utils.TenantContext;
 import multi_tenant.db.navigation.dto.CustomUserDetails;
@@ -19,7 +26,10 @@ import multi_tenant.db.navigation.dto.CustomUserDetails;
 public class CustomUserDetailsService implements UserDetailsService{
 
 	@Autowired
-    private GlobalUserRepository globalUserRepository;
+    private OwnerRepository ownerRepository;
+	
+	@Autowired
+	private DeveloperRepository developerRepository;
 	
 	@Autowired
 	private TenantUserRepository tenantUserRepository;
@@ -29,28 +39,54 @@ public class CustomUserDetailsService implements UserDetailsService{
 		
 		// Check header/tenant context to decide which repo to use
         String dbName = TenantContext.getCurrentTenant();
+        String userRole = TenantContext.getCurrentUserRole();
         if (dbName.equals("global_multi_tenant") || dbName.equals("default")) {
         	System.out.println("I am in Custom User Details Service, db name is pointing to global");
-        	// Use global repository
-            Owner globalUser = globalUserRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Global User Not found: " + email)); 
-            return new CustomUserDetails(
-            		globalUser.getEmail(),
-            		globalUser.getPassword(),
-            		Status.ACTIVE,
-            		true,
-            		dbName
-            		);
+        	System.out.println("Thread ID: "+ Thread.currentThread().getId() +" userRole "+ userRole);
+        	if(userRole.equals("DEVELOPER")) {
+        		// Use developer table
+        		Developer developer = developerRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Developer Not found: " + email));
+        		List<GrantedAuthority> authorities = new ArrayList<>();
+        		authorities.add(new SimpleGrantedAuthority("ROLE_DEVELOPER"));
+                return new CustomUserDetails(
+                		developer.getEmail(),
+                		developer.getPassword(),
+                		Status.ACTIVE,
+                		authorities,
+                		dbName
+                		);
+        	}else if(userRole.equals("OWNER")) {
+        		// Use owner table
+                Owner owner = ownerRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Owner Not found: " + email)); 
+                List<GrantedAuthority> authorities = new ArrayList<>();
+        		authorities.add(new SimpleGrantedAuthority("ROLE_OWNER"));
+        		authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+                return new CustomUserDetails(
+                		owner.getEmail(),
+                		owner.getPassword(),
+                		Status.ACTIVE,
+                		authorities,
+                		dbName
+                		);
+        		
+        	}
+        	System.out.println("I am in Custom User Details Service, no user found");
+        	return null;
+        	
+        
         }else {
         	System.out.println("I am in Custom user Details Service, dbName is "+ dbName);
         	try {
                 TenantUser tenantUser = tenantUserRepository.findByEmail(email)
                     .orElseThrow(() -> new UsernameNotFoundException("Tenant User Not found: " + email));
-
+                List<GrantedAuthority> authorities = new ArrayList<>();
+        		authorities.add(new SimpleGrantedAuthority("ROLE_"+tenantUser.getUserRole().name()));
                 return new CustomUserDetails(
                     tenantUser.getEmail(),
                     tenantUser.getPassword(),
                     tenantUser.getStatus(),
-                    false,
+                    authorities,
                     dbName
                 );
             } catch (Exception e) {
@@ -59,6 +95,7 @@ public class CustomUserDetailsService implements UserDetailsService{
                 throw e;
             }
         }
+
         
 	}
 
