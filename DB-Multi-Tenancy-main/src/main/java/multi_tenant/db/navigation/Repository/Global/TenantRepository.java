3501b3399fa14ebe4627d4250569a38d5ead53c0
ThuyKat <katiefirst7@gmail.com>
2025-03-03 14:51:42 +1100

    minor fix

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Repository/Global/TenantRepository.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Repository/Global/TenantRepository.java
@@ -1,9 +1,11 @@
 package multi_tenant.db.navigation.Repository.Global;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import multi_tenant.db.navigation.Entity.Global.Tenant;
 
+@Repository
 public interface TenantRepository extends JpaRepository<Tenant, Long> {
 	Tenant findByName(String name);
 }
