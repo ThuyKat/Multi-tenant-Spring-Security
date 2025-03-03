b4aaaa81ebfc396eef27d52306ba0d3f6d14186b
ThuyKat <katiefirst7@gmail.com>
2025-03-03 14:39:51 +1100

    added refresh cookie

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Config/CustomSuccessHandler.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Config/CustomSuccessHandler.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import multi_tenant.db.navigation.Service.CustomUserDetailsService;
@@ -36,21 +37,40 @@ public class CustomSuccessHandler implements AuthenticationSuccessHandler {
 		// Get authenticated user details
         UserDetails userDetails = userDetailsService.loadUserByUsername(authentication.getName());
 		
-        // Generate JWT token
+        // Generate JWT access token
         String jwt = jwtUtil.generateToken(userDetails);
+        // Generate refresh token
+        String refreshToken = jwtUtil.generateRefreshToken(userDetails);
         
         System.out.println("jwt: "+ jwt);
         
         //add jwt as a header
-        response.setHeader("Authorization", "Bearer " + jwt);
+        response.setHeader("Authorization", "Bearer " + jwt); // this only available for redirect request status code 302, then browser will generate totally new request to the redirect location which wont include this info in header
         
 //     // For testing - write the token to the response
 //        response.setContentType("application/json");
 //        PrintWriter writer = response.getWriter();
 //        writer.write("{\"jwt\": \"" + jwt + "\"}");
 //        writer.flush();
+     // Store JWT in session
+        request.getSession().setAttribute("jwt_token", jwt);// only for testing purpose that I add jwt in session
+        
+//        //Store JWT in cookie
+//        Cookie accessCookie = new Cookie("access_token", jwt);
+//        accessCookie.setHttpOnly(true);
+//        accessCookie.setPath("/");
+//        accessCookie.setMaxAge(3600*24); // 24 hrs-1day
+        
+        // Store refresh token in cookie
+        Cookie refreshCookie = new Cookie("refresh_token", refreshToken);
+        refreshCookie.setHttpOnly(true);
+        refreshCookie.setPath("/auth/refresh"); //only include the cookie in this path
+        refreshCookie.setMaxAge(604800); // 7 days
+        
         
         //send freresh + access token
-		response.sendRedirect("/tenant/test/welcome");
+        response.addCookie(refreshCookie);
+//        response.addCookie(accessCookie);
+		response.sendRedirect("/tenant/test/welcome"); 
 	}
 }
