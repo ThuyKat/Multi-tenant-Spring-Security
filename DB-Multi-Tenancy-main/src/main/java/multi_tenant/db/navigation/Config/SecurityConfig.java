53d9ffed4cb4f264fbce2b8e947b5af0956f6103
ThuyKat <katiefirst7@gmail.com>
2025-03-06 14:10:23 +1100

    remove form login, success handler, failure handler, added stateless for jwt, added request matchers

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Config/SecurityConfig.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Config/SecurityConfig.java
@@ -1,27 +1,16 @@
 package multi_tenant.db.navigation.Config;
 
-import java.io.IOException;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import multi_tenant.db.navigation.Service.CustomUserDetailsService;
-import multi_tenant.db.navigation.Service.TenantService;
-import multi_tenant.db.navigation.Utils.TenantContext;
 
 
 @Configuration
@@ -31,76 +20,45 @@ public class SecurityConfig {
 	CustomUserDetailsService customUserDetailsService;
 	
 	@Autowired
-	CustomSuccessHandler successHandler;
-	
-	@Autowired
 	private TenantFilter tenantFilter;
-	
-	@Autowired
-	private TenantService tenantService;
 	
 	@Autowired
 	private JwtFilter jwtFilter;
 	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-//		 return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		return NoOpPasswordEncoder.getInstance();
-	}
+	@Autowired CustomUsernamePasswordAuthenticationFilter loginFilter;
+	
+	
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
-		http.addFilterBefore(tenantFilter, UsernamePasswordAuthenticationFilter.class)
-		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+		
+		http
+	     // Tenant filter runs before JWT filter
+        .addFilterBefore(tenantFilter, UsernamePasswordAuthenticationFilter.class)
+		
+		// JWT filter runs after tenant filter but before authentication
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+   
 		.authorizeHttpRequests(
 				(requests) -> requests
-				.requestMatchers("/login/**")
-				.permitAll()
-				.anyRequest().authenticated())
-				.formLogin(
-				(formLogin) -> formLogin.permitAll().successHandler(successHandler)
-				.failureHandler((request, response, exception) -> {
-                    // Log detailed error
-                    System.out.println("Login failed: " + exception.getMessage());
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Login failed");
-                })
-				)
-				.logout(
-				(logout) -> logout.permitAll()
-				).csrf(csrf -> csrf.disable());
+				.requestMatchers("/api/login").permitAll()
+				.requestMatchers("/api/user/profile").hasAnyRole("GUEST", "ADMIN")
+				.requestMatchers("/api/admin/**").hasRole("ADMIN")
+				.requestMatchers("/api/owner/**").hasRole("OWNER")
+				.requestMatchers("/api/tenant/**").hasAnyRole("STAFF_LEVEL_1", "STAFF_LEVEL_2","ADMIN","SUPERVISOR")
+				.anyRequest().authenticated()) 
+		// logout is managed on client's side : remove JWT, redirect user to the login page
+		//can add server-side validation later (blacklist) .. to stop validate jwt after logout
+				.sessionManagement(session -> session
+			            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				        )
+				.csrf(csrf -> csrf.disable());
 		
 		return http.build();
 		
 	}
-//	   @Bean
-//	    public Filter tenantFilter() {
-//	        return (Filter) new OncePerRequestFilter() {
-//	            @Override
-//	            protected void doFilterInternal(HttpServletRequest request, 
-//	                                          HttpServletResponse response, 
-//	                                          FilterChain filterChain) throws ServletException, IOException {
-//	                
-//	            	String shopName = request.getHeader("shop-name");
-////	            	String shopName = request.getParameter("shop-name");
-//	                System.out.println("Interceptor: " + shopName);
-//	        		if(shopName != null) {
-//	        			String databaseName = tenantService.getDatabaseNameByShopId(shopName).getDbName();
-//	        			TenantContext.setCurrentTenant(databaseName);
-//	        			System.out.println("I am in tenantFilter method , setting DB "+databaseName);
-//
-//	        		}else {
-//	        			logger.warn("Shop-name header is missing");
-//	        			TenantContext.setCurrentTenant("databaseA");
-//	        			System.out.println("database A is set in threadID : "+ Thread.currentThread().getId()
-//	        					);
-//	        		}
-//	                
-//	                try {
-//	                    filterChain.doFilter(request, response);
-//	                } finally {
-//	                    TenantContext.clear();
-//	                }
-//	            }
-//	        };}
+	
+	
+
 }
