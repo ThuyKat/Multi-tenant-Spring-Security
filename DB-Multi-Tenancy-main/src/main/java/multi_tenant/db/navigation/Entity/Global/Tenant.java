bf16da99115181338e35ea830f05fef0957fd260
ThuyKat <katiefirst7@gmail.com>
2025-03-06 14:02:26 +1100

    switched rom json ignore to json managed/back ref

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Entity/Global/Tenant.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Entity/Global/Tenant.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -44,6 +46,7 @@ public class Tenant {
 	
 	@ManyToOne
 	@JoinColumn(name="owner_id")
+	@JsonBackReference
 	private Owner owner;
 	
 	public enum Status{
