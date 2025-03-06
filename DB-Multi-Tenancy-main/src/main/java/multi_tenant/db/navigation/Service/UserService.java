f1305c3fb4d2e622ea7963d6ca9fb6563239a314
ThuyKat <katiefirst7@gmail.com>
2025-03-06 14:06:58 +1100

    added method for testing purpose

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Service/UserService.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Service/UserService.java
@@ -1,5 +1,8 @@
 package multi_tenant.db.navigation.Service;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -12,7 +15,16 @@ public class UserService {
 	@Autowired
 	private TenantUserRepository userRespository;
 	
+	public List<TenantUser> getAllUsers(){
+		List<TenantUser> users = userRespository.findAll();
+		return users.isEmpty() ? Collections.emptyList() : users;
+	}
+	
 	public TenantUser getUserByEmail(String email) {
 		return userRespository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Tenant User Not found: " + email));
 	}
+	
+//	public TenantUser getUserByEmailWithPermission(String email) {
+//		return userRespository.findByEmailWithPermissions(email);
+//	}
 }
