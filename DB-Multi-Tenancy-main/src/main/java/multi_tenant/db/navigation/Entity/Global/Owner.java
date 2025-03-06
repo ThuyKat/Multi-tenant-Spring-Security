2a5b526f62a78131cb3488c05c122c8952dd8367
ThuyKat <katiefirst7@gmail.com>
2025-03-06 14:01:58 +1100

    switched from json ignore to json managed/back ref

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Entity/Global/Owner.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Entity/Global/Owner.java
@@ -9,12 +9,14 @@ import java.util.List;
 import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -54,7 +56,7 @@ public class Owner {
 	private LocalDateTime createdAt;
 	
 	@OneToMany (mappedBy = "owner", cascade = CascadeType.ALL) //owner lowecase, field in Tenant
-	@JsonIgnore
+	@JsonManagedReference
 	List<Tenant> tenants = new ArrayList<>();
 	
 	
