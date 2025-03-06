8f1574fc2bd8e81e91a5d387654c8fcba540b759
ThuyKat <katiefirst7@gmail.com>
2025-03-06 14:03:31 +1100

    added methods to switch tenant for owner

--- DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Repository/Global/OwnerRepository.java
+++ DB-Multi-Tenancy-main/src/main/java/multi_tenant/db/navigation/Repository/Global/OwnerRepository.java
@@ -2,9 +2,11 @@ package multi_tenant.db.navigation.Repository.Global;
 
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import multi_tenant.db.navigation.Entity.Global.Developer;
@@ -13,6 +15,9 @@ import multi_tenant.db.navigation.Entity.Global.Owner;
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner,Long> {
 
-	Optional<Owner> findByEmail(String username);
+	Optional<Owner> findByEmail(String email);
+	
+	@Query("SELECT o FROM Owner o LEFT JOIN FETCH o.tenants")
+    List<Owner> findAllWithTenants();
 
 }
